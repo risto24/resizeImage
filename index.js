1ecc4a88666fd940de2bfb2b6034dfdaeb829b9d
Risto Nishimura <risto24.contact@gmail.com>
2021-03-16 01:23:35 +0000

    update

--- index.js
+++ index.js
@@ -8,14 +8,14 @@ const resizeImage = (image) => {
   const parts = image.split(';');
   const mimType = parts[0].split(':')[1];
   const imageData = parts[1].split(',')[1];
-  const img = new Buffer(imageData, 'base64');
+  const img = new Buffer.from(imageData, 'base64');
   sharp(img)
     .resize(1000)
     .toBuffer()
     .then((resizedImageBuffer) => {
       const resizedImageData = resizedImageBuffer.toString('base64');
       const resizedBase64 = `data:${mimType};base64,${resizedImageData}`;
-      const buffer = Buffer.from(resizedBase64.substring(resizedBase64.indexOf(',') + 1));
+      const buffer = new Buffer.from(resizedBase64.substring(resizedBase64.indexOf(',') + 1));
       console.log('-----resized------')
       console.log("Byte: " + buffer.length);
       console.log("MB: " + buffer.length / 1e+6);
@@ -27,7 +27,7 @@ const resizeImage = (image) => {
 }
 
 
-const buffer = Buffer.from(image.substring(image.indexOf(',') + 1));
+const buffer = new Buffer.from(image.substring(image.indexOf(',') + 1));
 console.log('------target------')
 console.log("Byte: " + buffer.length);
 console.log("MB: " + buffer.length / 1e+6);
